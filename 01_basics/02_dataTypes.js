d260189d2b5dd57faf08a36928b53e70df811be3
Aditya Panigrahi <panigrahi0412@gmail.com>
2025-08-28 02:23:03 +0530

    add 01_basic folder

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3); // we are using nodejs not browser
+
+// code readablity should be high
+
+let name = "aditya";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not value assigned
+//symbol => unique
+
+// Object
+
+console.log(typeof "aditya"); // string
+console.log(typeof age); //  number
+// typeof null = object
+// typeof undefined = undefined

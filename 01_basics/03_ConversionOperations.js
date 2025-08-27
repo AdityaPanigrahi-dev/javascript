95288d3438211dc75bbe3735f60d5c6bfd4335c9
Aditya Panigrahi <panigrahi0412@gmail.com>
2025-08-28 03:27:42 +0530

    ConversionOperations

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -25,3 +25,28 @@ let someNumber = 22;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber); // string
+
+// ######### Operations ###########
+let value = 3;
+let negValue = -value;
+console.log(negValue); //-3
+
+let str1 = "Hello";
+let str2 = " Aditya";
+let str3 = str1 + str2;
+console.log(str3); // Hello Aditya
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); //12
+console.log("1" + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+

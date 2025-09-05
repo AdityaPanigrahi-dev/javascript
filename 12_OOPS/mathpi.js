5dad834023b2ca9919a1a15dad6e546ea92460e0
Aditya Panigrahi <panigrahi0412@gmail.com>
2025-09-05 12:26:37 +0530

    bind, more about objects

---
+++ 12_OOPS/mathpi.js
@@ -0,0 +1,29 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+
+//console.log(descriptor);
+
+const chai = {
+  name: "icetea",
+  price: 250,
+  isAvailable: true,
+
+  orderchai: function () {
+    console.log("chai nahi phati");
+  },
+};
+
+// console.log(chai);
+
+//console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+Object.defineProperty(chai, "name", {
+  //  writable: false,
+  enumerable: true,
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+  if (typeof value !== "function") {
+    console.log(`${key} : ${value}`);
+  }
+}
